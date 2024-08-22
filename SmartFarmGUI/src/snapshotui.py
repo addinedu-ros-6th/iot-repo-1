fa4574095e84e7c5a6983fff8a5e9ba8f2e32fca
kimboseong0602 <bo1739@naver.com>
2024-08-22 14:10:14 +0900

    Snapshot

---
+++ SmartFarmGUI/src/snapshotui.py
@@ -0,0 +1,13 @@
+import sys
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+from PyQt5 import uic
+from PyQt5.QtCore import *
+import cv2, imutils
+from DataManager import DataManager
+#from SerialCommunicator import Connector, Receiver, Sender 
+import pygame
+
+
+
+
