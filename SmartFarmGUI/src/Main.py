f13d439f94c0f0c50088c946b3ad54cc9a50b5fd
kimboseong0602 <bo1739@naver.com>
2024-08-22 11:37:46 +0900

    snapshot add to main

--- SmartFarmGUI/src/Main.py
+++ SmartFarmGUI/src/Main.py
@@ -5,10 +5,31 @@ from PyQt5 import uic
 from PyQt5.QtCore import *
 import cv2, imutils
 from DataManager import DataManager
-from SerialCommunicator import Connector, Receiver, Sender 
+import datetime
+#from SerialCommunicator import Connector, Receiver, Sender 
 import pygame
+import numpy as np
+import time
+import os
 
-from_class = uic.loadUiType("/home/mr/dev_ws/iot_project/ui/main.ui")[0]
+from_class = uic.loadUiType("iot-repo-1/SmartFarmGUI/ui/main.ui")[0]
+
+class Camera(QThread):
+    update = pyqtSignal()
+
+    def __init__(self, sec = 0, parent = None):
+        super().__init__()
+        self.main = parent
+        self.running = True
+
+    def run(self):
+        count = 0
+        while self.running == True:
+            self.update.emit()
+            time.sleep(0.1)
+
+    def stop(self):
+        self.running = False
 
 class WindowClass(QMainWindow, from_class):
 
@@ -20,25 +41,61 @@ class WindowClass(QMainWindow, from_class):
     self.setupUi(self)
     self.label_system_message.hide()
     self.btn_harvest.hide()
+    self.camera = Camera(self)
+    self.count = 0
 
+    self.pixmap = QPixmap()
+  
     self.btn_start.clicked.connect(self.onClick_select_crop)
     self.btn_massage.clicked.connect(self.onClick_play_massage)
     self.btn_loveVoice.clicked.connect(self.onClick_play_love_voice)
+    self.btn_snapshot.clicked.connect(self.snapshotwindow)
+    
+    self.cameraStart()
+    self.camera.update.connect(self.updateCamera)
 
     self.environment_parameters = {}
 
-    # 포트와 통신을 위한 Thread 객체 생성
-    self.connector = Connector()
-    self.farm_sensor_polling_thread = Sender(1, self.update_get_env_data)
-    self.receiver = Receiver(self.connector.conn)
-    self.receiver.received_env_value.connect(self.set_env_cur_value)
-    self.receiver.received_env_io_result.connect(self.update_env_io_icon)
+  def snapshotwindow(self):
+    if hasattr(self, 'image'):
+        window_2 = snapshotui(self.image, self)
+        window_2.exec_()
 
-    self.login()
+    # 포트와 통신을 위한 Thread 객체 생성
+    #self.connector = Connector()
+    #self.farm_sensor_polling_thread = Sender(1, self.update_get_env_data)
+    #self.receiver = Receiver(self.connector.conn)
+    #self.receiver.received_env_value.connect(self.set_env_cur_value)
+    #self.receiver.received_env_io_result.connect(self.update_env_io_icon)
+#
+    #self.login()
 
   # 키우는 식물 여부에 따라 UI 활성/비활성하고
   # 키우는 식물이 없는 경우 새로운 plant_data를 추가하고
   # 해당 식물의 정보(권장 온도/습도 등)를 가져온다.
+
+  def cameraStart(self):
+        self.camera.running = True
+        self.camera.start()
+        self.video = cv2.VideoCapture(-1)
+
+  def updateCamera(self):
+        #self.label.setText('Camera Running : ' + str(self.count))
+        #self.count += 1
+        retval, self.image = self.video.read()
+        if retval:
+            self.image = cv2.cvtColor(self.image, cv2.COLOR_BGR2RGB)
+
+            h,w,c = self.image.shape
+            qimage = QImage(self.image.data, w, h, w*c, QImage.Format_RGB888)
+
+            self.pixmap = self.pixmap.fromImage(qimage)
+            self.pixmap = self.pixmap.scaled(self.plantcam.width(), self.plantcam.height())
+
+            self.plantcam.setPixmap(self.pixmap)
+
+        self.count +=1
+
   def login(self):
     grow_data = self.db.get_growing_plant_data()
 
@@ -99,7 +156,7 @@ class WindowClass(QMainWindow, from_class):
     self.label_recom_temp.setText("/ " + str(recommend_temperature))
     self.label_recom_humidity.setText("/ " + str(recommend_humidity))
     self.label_recom_light.setText("/ " + str(recommend_light))
-
+  
   
   # 온도/습도/빛의 값이 변경될때마다 호출
   def set_env_cur_value(self, data):
@@ -199,14 +256,14 @@ class WindowClass(QMainWindow, from_class):
 
   def onClick_play_love_voice(self):
     # self.stop_audio()
-    audio_file = "/home/mr/dev_ws/iot_project/resource/loveVoice.mp3"
+    audio_file = "iot-repo-1/SmartFarmGUI/resource/loveVoice.mp3"
     pygame.mixer.music.load(audio_file)
     pygame.mixer.music.play()
     return
 
   def onClick_play_massage(self):
     # self.stop_audio()
-    audio_file = "/home/mr/dev_ws/iot_project/resource/massage.mp3"
+    audio_file = "iot-repo-1/SmartFarmGUI/resource/massage.mp3"
     pygame.mixer.music.load(audio_file)
     pygame.mixer.music.play()
 
@@ -248,6 +305,46 @@ class WindowClass(QMainWindow, from_class):
     if pygame.mixer.music.get_busy():
       pygame.mixer.music.stop()
 
+class snapshotui(QDialog):
+
+    def __init__(self, image, parent=None):
+        super().__init__(parent)
+        uic.loadUi('iot-repo-1/SmartFarmGUI/ui/snapshot.ui', self)
+        self.show()
+        self.pixmap = QPixmap()
+        self.SAVEbutton.clicked.connect(self.capture)
+        self.notSAVEbutton.clicked.connect(self.notsave)
+
+        h, w, c = image.shape
+        qimage = QImage(image.data, w, h, w * c, QImage.Format_RGB888)
+        self.pixmap = self.pixmap.fromImage(qimage)
+        self.pixmap = self.pixmap.scaled(self.snapshot.width(), self.snapshot.height(), Qt.KeepAspectRatio)
+        # Label에 pixmap 설정
+        self.snapshot.setPixmap(self.pixmap)
+
+    def capture(self):
+        self.now = datetime.datetime.now().strftime('%Y%m%d_%H%M%S')
+        filename = f'{self.now}.png'
+
+        folder_path = 'iot-repo-1/Snapshot'  # 원하는 폴더 경로로 변경하세요
+        if not os.path.exists(folder_path):
+            os.makedirs(folder_path)  # 폴더가 존재하지 않으면 생성
+        
+        file_path = os.path.join(folder_path, filename)  # 전체 파일 경로 생성
+
+        # QPixmap을 QImage로 변환
+        image = self.pixmap.toImage()
+        
+        # QImage를 파일로 저장
+        image.save(file_path, "PNG")
+        self.to_plant_message = self.message2plant.text()
+        print("파일경로 : ", file_path)
+        print("현재 시간 : ",self.now)
+        print("메세지 : ", self.to_plant_message)
+        self.close()
+
+    def notsave(self):
+        self.close()
 
 if __name__ == "__main__":
   app = QApplication(sys.argv)
